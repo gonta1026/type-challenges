6caecd9c0675e458ef9c852826326ef66629a0eb
gonta1026 <katano.original@gmail.com>
2021-10-02 00:20:10 +0900

    prettierの修正

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,25 +1,25 @@
 declare namespace Utilty {
   // 指定をしたKeysで指定をしたユニオンをnullを合わせたユニオン型へする変更をさせる。
   export type TargetNullable<T, Keys extends keyof T> = {
-    [P in keyof T]: Keys extends P ? T[P] | null : T[P]
-  }
+    [P in keyof T]: Keys extends P ? T[P] | null : T[P];
+  };
   export type SelectChange<T, Keys extends keyof T, U> = {
-    [P in keyof T]: P extends Keys ? U : T[P]
-  }
+    [P in keyof T]: P extends Keys ? U : T[P];
+  };
   // ユニオンからnullを消す
   export type DeleteNull<T> = {
-    [P in keyof T]: Exclude<T[P], null>
-  }
+    [P in keyof T]: Exclude<T[P], null>;
+  };
   // keyofのvalueバージョン
-  export type valueOf<T> = T[keyof T]
+  export type valueOf<T> = T[keyof T];
   // 指定したプロパティををオプショナルに変更する
   export type PartiallyPartial<T, K extends keyof T> = Partial<Pick<T, K>> &
-    Pick<T, Exclude<keyof T, K>>
+    Pick<T, Exclude<keyof T, K>>;
 
   // 例： type result = TargetPropertyNames<Part, Function> // "add"
   export type TargetPropertyNames<T, U> = {
-      [P in keyof T]: T[P] extends U ? P : never
-    }[keyof T]
+    [P in keyof T]: T[P] extends U ? P : never;
+  }[keyof T];
   // 関数ならなんでもOK
-  export type voidFunc = (...args: any[]) => any
+  export type voidFunc = (...args: any[]) => any;
 }
