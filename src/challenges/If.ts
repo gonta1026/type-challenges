e450d95c50f1cb55f140a28c05dadacff3f290e6
gonta1026 <katano.original@gmail.com>
2022-01-04 08:13:38 +0900

    fix

---
+++ src/challenges/If.ts
@@ -0,0 +1,4 @@
+// 条件分に合わせて型を返す
+type If<C, T, F> = C extends true ? T : C extends false ? F : Error;
+type A = If<true, "a", "b">; // expected to be 'a'
+type B = If<false, "a", "b">; // expected to be 'b'
